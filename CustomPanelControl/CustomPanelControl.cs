fc6b477fb16d9e364902944e832d9d3b4e9eeac6
Brahn Partridge <github@fushnisoft.com>
2014-06-12 21:12:22 +0200

    first version

---
+++ CustomPanelControl/CustomPanelControl.cs
@@ -0,0 +1,18 @@
+﻿
+using SoftVelocity.ClarionNet.CommonProperties;
+using SoftVelocity.ClarionNet.Windows;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+namespace Fushnisoft.CustomClarionIDEControls
+{
+    [WindowDesignerClassAttribute, Designer(typeof(PanelControlDesigner), typeof(IDesigner)), ToolboxBitmap(typeof(Panel_CUSTOM), "Resources.grid.png")]
+    public class Panel_CUSTOM : SoftVelocity.ClarionNet.Windows.PANEL
+    {
+        public Panel_CUSTOM()
+        {
+            Bevel.Outer = 0;
+        }
+
+    }
+}
